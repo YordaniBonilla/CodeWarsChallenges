39d98876898ce0835dd6f27159c2fee5cbb41743
YordaniBonilla <b.yordani93@gmail.com>
2019-08-03 20:52:15 -0500

    19th

---
+++ 8kyu/removeStringSpace.js
@@ -0,0 +1,8 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+// input string with spaces
+//output string with no spaces
+//constrainst 0
+// edgec cases 0
+function noSpace(x) {
+    return x.split(' ').join('')
+}
